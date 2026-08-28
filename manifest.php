d8a92ec52ccf9e36202072710d5820e3b675b06e
jonmlas <jonmlas@gmail.com>
2026-08-28 22:02:24 +0800

    Version bump

--- manifest.php
+++ manifest.php
@@ -11,7 +11,7 @@ $manifest['description'] = __(
 	'fw'
 );
 
-$manifest['version']     = '1.0.54';
+$manifest['version']     = '1.0.55';
 $manifest['display']     = true;
 $manifest['standalone']  = true;
 $manifest['thumbnail']   = 'thumbnail.svg';
